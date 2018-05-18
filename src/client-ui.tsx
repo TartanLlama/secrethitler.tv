6f3ecbc294d5ff4eb9c445ada4412a02e0e53ff5
Simon Brand <simon@codeplay.com>
2018-05-18 13:39:31 +0100

    Forgot file

---
+++ src/client-ui.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+export = 0;
+
+ReactDOM.render(
+  <form>
+  Name: <input type="text"/><br/>
+<input type="button" value="Register"/>   
+  </form>
+  ,
+  document.getElementById('root')
+);
