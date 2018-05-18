be76569fe063ce97913227c619b3927565d5edc2
Simon Brand <simon@codeplay.com>
2018-05-18 16:20:37 +0100

    Get closer to compiling

--- src/client-ui.tsx
+++ src/client-ui.tsx
@@ -5,10 +5,7 @@ import * as ReactDOM from "react-dom";
 
 var ws: Nes.Client;
 
-interface NameFormState {
-    value: string;
-}
-class NameForm extends React.Component<NameFormState> {
+class NameForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {value: ''};
@@ -17,12 +14,12 @@ class NameForm extends React.Component<NameFormState> {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event: React.FormEvent<HTMLInputElement>) {
-    this.setState({value: event.currentTarget.value});
+  handleChange(event) {
+    this.setState({value: event.target.value});
   }
 
-  *handleSubmit(event) {
-    ws = Nes.Client('ws://localhost:3000');
+  async handleSubmit(event) {
+    ws = new Nes.Client('ws://localhost:3000');
     const response = await ws.request('register');
     console.log(response);
   }
@@ -32,7 +29,7 @@ class NameForm extends React.Component<NameFormState> {
       <form onSubmit={this.handleSubmit}>
         <label>
           Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="text" value={this.state['value']} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Register" />
       </form>
